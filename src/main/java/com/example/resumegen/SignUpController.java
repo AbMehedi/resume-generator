c281f7a4b6dfabe0f3d75a80fd3ca5a0fa9531ad
rayan-rah57 <rayan.rah57@gmail.com>
2025-04-22 23:23:12 +0600

    signup page

---
+++ src/main/java/com/example/resumegen/SignUpController.java
@@ -0,0 +1,20 @@
+package com.example.resumegen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    @FXML
+    private TextField Username;
+    @FXML
+    private TextField Password;
+    @FXML
+    private TextField Email;
+    @FXML
+    private TextField ConfirmPassword;
+    @FXML
+    private Button SignUp;
+    @FXML
+    private Button Login;
+}
