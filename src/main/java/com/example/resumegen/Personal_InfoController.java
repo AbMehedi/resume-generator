87588e002d6033671179bded760853150956cb2b
Omor Faruck Ullas <omor.farukh16@gmail.com>
2025-04-23 06:56:55 +0600

    OmorFrontend

--- src/main/java/com/example/resumegen/Personal_InfoController.java
+++ src/main/java/com/example/resumegen/Personal_InfoController.java
@@ -1,9 +1,14 @@
 package com.example.resumegen;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Personal_InfoController {
 
     private Label label;
     private TextField FirstName;
+    private TextField LastName;
+    private TextField Email;
+    private TextField Phone;
+
 }
